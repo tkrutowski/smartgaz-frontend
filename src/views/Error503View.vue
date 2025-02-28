825c5afca16fda8380e1ff5919812def9150d839
Tomek <tkrutowski@gmail.com>
2025-02-28 19:24:06 +0100

    fix error503

--- src/views/Error503View.vue
+++ src/views/Error503View.vue
@@ -1,13 +1,40 @@
 <script setup lang="ts">
 import TheMenu from '../components/TheMenu.vue'
+import {useAuthorizationStore} from "@/stores/authorization.ts";
+import {onMounted, ref} from "vue";
+import router from "@/router";
+
+const authorizationStore = useAuthorizationStore();
+const counter = ref<number>(5);
+
+const startPingLoop = () => {
+  const interval = setInterval(() => {
+    if (counter.value > 0) {
+      counter.value--;
+    } else {
+      authorizationStore.testPing()
+          .then(() => {
+            clearInterval(interval);
+            router.back();
+          })
+          .catch(() => {
+            counter.value = 5;
+          });
+    }
+  }, 1000);
+};
+
+onMounted(() => {
+  console.log("onMounted() Error503");
+  startPingLoop();
+});
+
 </script>
 
 <template>
   <TheMenu/>
   <div class="flex flex-col justify-center items-center w-full mt-10 text-primary">
     <p class="text-3xl">Serwis chwilowo niedostępny</p>
-    <p class="text-lg mt-3">Proszę spróbować za chwilę...</p>
+    <p class="text-lg mt-3">Ponowne sprawdzenie za {{counter}} sekund</p>
   </div>
 </template>
-
-<style scoped></style>
